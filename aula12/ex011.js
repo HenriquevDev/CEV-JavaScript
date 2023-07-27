4f75b2a27c34e15cfe01a7e6b875f310546e367e
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-07-27 19:52:48 -0300

    .

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 16
+console.log(`Você tem ${idade} anos`)
+if (idade < 16){
+    console.log(`Não Vota`)
+} else if(idade < 18 || idade > 67 ){
+     {
+        console.log(`Voto Opcional`)
+    } 
+} else{
+    console.log(`O voto é obrigatório`)
+}
